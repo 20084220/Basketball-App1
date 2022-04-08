a4a0b32b805d0ddef761822c31d72229865f865e
scott <20084220@mail.wit.ie>
2022-04-08 13:51:42 +0100

    list methods and add methods where added

---
+++ src/main/kotlin/controller/PlayerAPI.kt
@@ -0,0 +1,41 @@
+package controller
+
+import models.Player
+
+
+class PlayerAPI
+{
+
+    private var players = ArrayList<Player>()
+
+  //crud methods add, delete update
+    fun add(player: Player): Boolean {
+        return players.add(player)
+    }
+    fun numberOfPlayers(): Int {
+        return players.size
+    }
+    fun listAllPlayers(): String =
+        if  (players.isEmpty()) "No players stored"
+        else formatListString(players)
+
+//search for my player by name
+    fun searchByName (searchString : String) =
+        formatListString(
+            players.filter { player -> player.playerName.contains(searchString, ignoreCase = true) })
+    //list the players by team
+    fun listByTeam(teamName : String) =
+        formatListString(
+            players.filter { player -> player.team.equals(teamName, ignoreCase = true) })
+
+    fun listByRetired(retiredPlayer : String) =
+        formatListString(
+            players.filter { player -> player.retired. })
+
+    //to-string in a nice way
+    private fun formatListString(playerToFormat : List<Player>) :String =
+        playerToFormat
+            .joinToString (separator = "\n") { player ->
+                players.indexOf(player).toString() + ": " + player.toString() }
+
+}
--- src/main/kotlin/models/player.kt
+++
@@ -1,3 +0,0 @@
-package models
-data class player(var playerName: String, var playerNum: Int, var team : String,var height: Int,var position: String, var retired :Boolean){
-}
